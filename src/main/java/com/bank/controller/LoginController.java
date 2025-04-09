8a050d7007471b749159668e31e7dcda13260c4a
Frank <fcarpio113@gmail.com>
2025-04-09 13:12:38 -0500

    Falta verificar

--- src/main/java/com/bank/controller/LoginController.java
+++ src/main/java/com/bank/controller/LoginController.java
@@ -5,19 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.dto.TarjetaDTO;
-import com.bank.security.jwt.JwtUtils;
 import com.bank.service.LoginService;
-import com.bank.serviceImpl.TarjetaServiceImpl;
 
 @RestController
 @RequestMapping("/login")
