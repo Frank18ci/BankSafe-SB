3860d100b04699488ec9321d5dfd1a20b5900e42
falas01 <rusbelramos317@gmail.com>
2025-04-10 18:18:45 -0500

    Tipo Tarjeta

--- src/main/java/com/bank/serviceImpl/PrestamoServiseImpl.java
+++ src/main/java/com/bank/serviceImpl/PrestamoServiseImpl.java
@@ -2,11 +2,18 @@ package com.bank.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.bank.dto.PrestamoDTO;
+import com.bank.dto.UserDTO;
+import com.bank.model.Prestamo;
+import com.bank.repository.PrestamoRepository;
 import com.bank.service.PrestamoService;
-
+@Service
 public class PrestamoServiseImpl implements PrestamoService {
-
+	@Autowired
+	private PrestamoRepository prestamoRepository;
 	@Override
 	public List<PrestamoDTO> list() {
 		// TODO Auto-generated method stub
